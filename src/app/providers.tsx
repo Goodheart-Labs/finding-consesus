b10ea0a5452ff72b891252c43fdf47cd44d25774
nathanpmyoung <nathanpmyoung@gmail.com>
2024-08-29 17:41:47 +0100

    add posthog

---
+++ src/app/providers.tsx
@@ -0,0 +1,21 @@
+"use client";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
+
+if (!process.env.NEXT_PUBLIC_POSTHOG_KEY) {
+  throw new Error("NEXT_PUBLIC_POSTHOG_KEY is not set");
+}
+
+if (!process.env.NEXT_PUBLIC_POSTHOG_HOST) {
+  throw new Error("NEXT_PUBLIC_POSTHOG_HOST is not set");
+}
+
+if (typeof window !== "undefined") {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+  });
+}
+export function CSPostHogProvider({ children }: { children: React.ReactNode }) {
+  return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
+}
